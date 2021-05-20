b852277111c8fa1135fdaa3e2349a25afb9b4198
GrapeJUICE1 <anonymyty123@gmail.com>
2021-05-20 17:47:37 +0600

    removed unecessary logs

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,9 +13,7 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
   res.cookie('jwt', token, {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + 50 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   });
